5b4d9c4e0366bd3e16ef43f6249f961f2d1e0c7f
bergold <emil.bergold@outlook.com>
2014-01-28 20:08:58 +0100

    update editor.js, app.html

--- scripts/app/editor.js
+++ scripts/app/editor.js
@@ -1,3 +1,32 @@
-define(['lib/codemirror', 'settings'], function(codemirror, settings) {
+define(['lib/jquery', 'lib/codemirror', 'settings', 
+        'mode/clike', 'mode/css', 'mode/htmlmixed', 'mode/javascript', 
+        'mode/perl', 'mode/php', 'mode/ruby', 'mode/sql', 'mode/xml'], 
+       function($, codemirror, settings) {
+    
+    var editor = {
+        
+        myEditor: codemirror($("#editor")),
+        
+        setDoc: function(doc, mode) {      // sets the document or creates a new one
+            doc = typeof doc === "string" ? codemirror.Doc(doc, mode) : doc;
+            return this.myEditor.swapDoc(doc);
+        },
+        
+        setTheme: function(theme) {          // sets the theme of the editor
+            this.myEditor.setOption("theme", theme);
+        },
+        
+        doc: function() {                  // returns the active document
+            return this.myEditor.getDoc();
+        }
+        
+    };
+    
+    return {
+        cm: editor.myEditor,
+        setDoc: editor.setDoc.bind(editor),
+        setTheme: editor.setTheme.bind(editor),
+        doc: editor.doc.bind(editor)
+    };
     
 });
