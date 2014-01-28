8772a1a589187a98c5d5c7ddb54e9ef2df8c991e
bergold <emil.bergold@outlook.com>
2014-01-28 18:43:13 +0100

    Implemented the communication module

--- scripts/app/communication.js
+++ scripts/app/communication.js
@@ -1,3 +1,27 @@
 define(function() {
     
+    var cmd = {
+        
+        buffer: {},
+        
+        addListener: function (a, b) {
+            if (undefined === buffer[a])
+                buffer[a] = [];
+            buffer[a].push(b);
+        },
+        
+        trigger: function(a, b) {
+            var c = buffer[a] || [];
+            for (var i = 0; i < c.lenght; i++) {
+                c[i](b);
+            }
+        }
+        
+    };
+    
+    return {
+        addListener: cmd.add.bind(cmd),
+        trigger: cmd.trigger.bind(cmd)
+    };
+    
 });
