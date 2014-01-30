e597357e825da8f450d9be79ada1e3f9441a4ad6
bergold <emil.bergold@outlook.com>
2014-01-30 22:02:06 +0100

    implemented function framework to project.js

--- scripts/app/project.js
+++ scripts/app/project.js
@@ -1,3 +1,87 @@
 define(['storage', 'localfile', 'remotefile', 'communication'], function(storage, lfile, rfile, cmd) {
     
+    var cP = function(name) {
+        this._name = name;
+        this._dir = null;
+        this._config = {};
+        this._local = undefined;
+        this.ready = null;
+    };
+    
+    cP.prototype.constructor = cP;
+    
+    cP.prototype.init = function() {
+        
+    };
+    
+    cP.prototype.save = function() {
+        
+    };
+    
+    cP.prototype.isLocal = function() {
+        return this._local;
+    };
+    cP.prototype.isRemote = function() {
+        return this._local === undefined ? undefined : !this._local;
+    };
+    
+    cP.prototype.getLabel = function() {
+        
+    };
+    cP.prototype.getFullPath = function() {
+        
+    };
+    cP.prototype.getFiletree = function() {
+        
+    };
+    
+    cP.prototype.readFile = function() {
+        
+    };
+    cP.prototype.writeFile = function() {
+        
+    };
+    
+    cP.prototype.create = function() {
+        
+    };
+    cP.prototype.rename = function() {
+        
+    };
+    cP.prototype.remove = function() {
+        
+    };
+    
+    
+    var projects = {
+        
+        buffer: {},
+        
+        getList: function() {
+            
+        },
+        
+        open: function(name) {
+            if (buffer[name]) {
+                return buffer[name];
+            } else {
+                var p = new cP(name);
+                p.init();
+                this.buffer[name] = p;
+                return p;
+            }
+        },
+        
+        remove: function(name) {
+            
+        }
+        
+    };
+    
+    return {
+        list: projects.getList.bind(projects),
+        open: projects.open.bind(projects),
+        remove: projects.remove.bind(projects)
+    };
+    
 });
