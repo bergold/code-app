02b78c94767916473b1c1ea28afb3d0745560ad5
bergold <emil.bergold@outlook.com>
2014-01-30 18:00:13 +0100

    update localfile.js

--- scripts/app/localfile.js
+++ scripts/app/localfile.js
@@ -1,3 +1,60 @@
 define(function() {
     
+    var lfile = {
+        
+        chooseDir: function() {
+            
+        },
+        
+        readDir: function() {
+            
+        },
+        
+        createDir: function() {
+            
+        },
+        
+        removeDir: function() {
+            
+        },
+        
+        renameDir: function() {
+            
+        },
+        
+        readFile: function() {
+            
+        },
+        
+        createFile: function() {
+            
+        },
+        
+        deleteFile: function() {
+            
+        },
+        
+        renameFile: function() {
+            
+        },
+        
+        writeFile: function() {
+            
+        }
+        
+    };
+    
+    return {
+        chooseDir: lfile.chooseDir.bind(lfile),
+        readDir: lfile.readDir.bind(lfile),
+        createDir: lfile.createDir.bind(lfile),
+        removeDir: lfile.removeDir.bind(lfile),
+        renameDir: lfile.renameDir.bind(lfile),
+        readFile: lfile.readFile.bind(lfile),
+        createFile: lfile.createFile.bind(lfile),
+        deleteFile: lfile.deleteFile.bind(lfile),
+        renameFile: lfile.renameFile.bind(lfile),
+        writeFile: lfile.writeFile.bind(lfile),
+    };
+    
 });
